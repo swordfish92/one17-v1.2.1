5ad7adf38855c6211f0f6e86637101a9c73d2f34
swordfish92 <32511973+swordfish92@users.noreply.github.com>
2026-07-14 07:18:22 +0100

    Refactor app listening and add logging for URL
    Refactor app listening to use a variable for port and log the URL.

--- one17-v1.2.1/src/main.ts
+++ one17-v1.2.1/src/main.ts
@@ -103,6 +103,12 @@ async function bootstrap() {
     SwaggerModule.setup('api-docs', app, document);
   }
 
-  await app.listen(process.env.PORT ?? 3000);
+ const port = Number(process.env.PORT) || 3000;
+
+await app.listen(port, '0.0.0.0');
+
+console.log(`Application is running on port ${port}`);
+console.log(await app.getUrl());
+
 }
 bootstrap();
